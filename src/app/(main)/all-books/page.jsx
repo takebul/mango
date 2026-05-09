7b7ab250f15df72de0b96626365cf20573459fc7
takebul <takebulislam@gmail.com>
2026-05-09 21:53:53 +0600

    update search empty section. added a empty message

--- src/app/(main)/all-books/page.jsx
+++ src/app/(main)/all-books/page.jsx
@@ -2,7 +2,10 @@ import AllBooks from "@/components/books-page/AllBooks";
 import CategorySidebar from "@/components/books-page/CategorySidebar";
 import SearchBookInputForm from "@/components/books-page/SearchBookInputForm";
 import { getBooks, getSearchBooks } from "@/lib/data";
+import { Button } from "@heroui/react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa6";
 
 const AllBooksPage = async ({ searchParams }) => {
   const { category, search } = await searchParams;
@@ -22,7 +25,7 @@ const AllBooksPage = async ({ searchParams }) => {
       {books.length === 0 ? (
         <div className="text-center my-10">
           <DotLottieReact
-            className="w-120 mx-auto"
+            className="mx-auto sm:w-120"
             src="https://lottie.host/638c449a-0631-4f58-a4e4-dba49a8110c1/1WnnkogHtf.lottie"
             loop
             autoplay
@@ -30,7 +33,14 @@ const AllBooksPage = async ({ searchParams }) => {
           <h2 className="text-4xl font-bold">
             This book is not available here.
           </h2>
-          <p className="pt-1">Please try another one</p>
+          <p className="pt-1 text-xl">Please try another one</p>
+          <div className="mt-2">
+            <Link href={"/all-books"}>
+              <Button>
+                <FaArrowLeft /> Previews page
+              </Button>
+            </Link>
+          </div>
         </div>
       ) : (
         <div className="grid md:grid-cols-6 gap-4 p-4">
